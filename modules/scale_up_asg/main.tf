0e26e821aeba424163d9f1855aac50e5ae192704
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2022-06-28 12:04:41 +0000

    remove time zone from code

--- modules/scale_up_asg/main.tf
+++ modules/scale_up_asg/main.tf
@@ -5,7 +5,6 @@ resource "aws_autoscaling_schedule" "bastion_dfp_scale_up" {
   desired_capacity       = "${var.desired_capacity}"
   recurrence             = "${var.scale_up_cron}"
   autoscaling_group_name = "${data.aws_autoscaling_group.bastion_dfp_asg.name}"
-  time_zone              = "${var.time_zone}"
 }
 
 resource "aws_autoscaling_schedule" "api_dfp_scale_up" {
@@ -15,5 +14,4 @@ resource "aws_autoscaling_schedule" "api_dfp_scale_up" {
   desired_capacity       = "${var.desired_capacity}"
   recurrence             = "${var.scale_up_cron}"
   autoscaling_group_name = "${data.aws_autoscaling_group.api_dfp_asg.name}"
-  time_zone              = "${var.time_zone}"
 }
